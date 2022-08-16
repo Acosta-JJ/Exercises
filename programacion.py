edcf9ebe5ec3dca31057cadfc3f24a1a8d671e70
acosta-JJ <acostasantana.jj@gmail.com>
2022-08-16 13:18:07 -0700

    Get less visited question works

--- programacion.py
+++ programacion.py
@@ -13,11 +13,16 @@ def getNotAnsweredQuestions(data):
 def getAnsweredQuestions(data):
     return list(filter(lambda x:x['is_answered'], data['items']))
 
+
+def lessVisitedQuestion(data):
+    return min(data['items'], key=lambda x:x['view_count'])
+
 if __name__ == "__main__":
     url="https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=perl&site=stackoverflow"
     data = getJSON(url)
     answeredQuestions = getAnsweredQuestions(data)
-    print("The amount of answered questions is: \n" + str(len(answeredQuestions)))
+    print("The amount of answered questions is: " + str(len(answeredQuestions)))
     notAnsweredQuestions = getNotAnsweredQuestions(data)
-    print("The amount of NOT answered questions is: \n" + str(len(notAnsweredQuestions)))
-    
+    print("The amount of NOT answered questions is: " + str(len(notAnsweredQuestions)))
+    lessVisited = lessVisitedQuestion(data)
+    print("The less visited question has this visits: " + str(lessVisited['view_count']))
