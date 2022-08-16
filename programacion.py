1d24e7ef0cda7d4b3b1787c0b867982a49cb881e
acosta-JJ <acostasantana.jj@gmail.com>
2022-08-16 13:29:46 -0700

    Get the owner with the best reputation works

--- programacion.py
+++ programacion.py
@@ -25,6 +25,10 @@ def getOldestQuestion(data):
 def getNewestQuestion(data):
     return max(data['items'], key=lambda x:x['creation_date'])
 
+
+def getOwnerHighestReputation(data):
+    return max(data['items'], key=lambda x:x['owner']['reputation'] )
+
 if __name__ == "__main__":
     url="https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=perl&site=stackoverflow"
     data = getJSON(url)
@@ -38,4 +42,6 @@ if __name__ == "__main__":
     print("The oldest question has this tittle: " + oldestQuestion['title'])
     newestQuestion = getNewestQuestion(data)
     print("The newest question has this tittle: " + newestQuestion['title'])
+    ownerHighestReputation = getOwnerHighestReputation(data)
+    print("The owner with high reputation is: " + ownerHighestReputation['owner']['display_name'] + " and the reputation score is: "+ str(ownerHighestReputation['owner']['reputation']))
 
