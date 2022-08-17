a3bd7bf5bbd607602afffafb116f82d8914deb8d
acosta-JJ <acostasantana.jj@gmail.com>
2022-08-17 05:04:46 -0700

    Test for Best rated owner implemented

--- test.py
+++ test.py
@@ -3,11 +3,11 @@ from programacion import *
 
 class TestMethods(unittest.TestCase):
     def setUp(self):
-        self.dict = {"items":[{"creation_date":4,"owner":[{"reputation":0}],"is_answered":True,"view_count":99},
-        {"creation_date":0,"owner":[{"reputation":1}],"is_answered":True,"view_count":3},
-        {"creation_date":1,"owner":[{"reputation":2}],"is_answered":True,"view_count":9999},
-        {"creation_date":2,"owner":[{"reputation":3}],"is_answered":True,"view_count":15},
-        {"creation_date":3,"owner":[{"reputation":4}],"is_answered":False,"view_count":79},]}
+        self.dict = {"items":[{"creation_date":4,"owner":{"reputation":0},"is_answered":True,"view_count":99},
+        {"creation_date":0,"owner":{"reputation":1},"is_answered":True,"view_count":3},
+        {"creation_date":1,"owner":{"reputation":2},"is_answered":True,"view_count":9999},
+        {"creation_date":2,"owner":{"reputation":3},"is_answered":True,"view_count":15},
+        {"creation_date":3,"owner":{"reputation":999},"is_answered":False,"view_count":79}]}
 
     def test_getNotAnsweredQuestions(self):
         self.assertEqual(len(getNotAnsweredQuestions(self.dict)),1)
@@ -23,8 +23,7 @@ class TestMethods(unittest.TestCase):
 
     def test_getNewestQuestion(self):
         self.assertEqual(getNewestQuestion(self.dict)['creation_date'],4)
-        
 
-    '''def test_default_widget_size(self):
-        self.assertEqual(self.widget.size(), (50,50),
-                         'incorrect default size')'''
+    def test_getHighestOwnerReputation(self):
+        self.assertEqual(getOwnerHighestReputation(self.dict)['owner']['reputation'],999)
+        
